fbfa545442434e5be8bf13fb241c3da983e6b693
oussamabelhouchett <oussama.belhouchette@gmail.com>
2020-11-26 21:54:36 +0100

    integrate template Accuiel

---
+++ src/main/webapp/app/accueil-right-section/accueil-right-section.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { BeOpenMindAppSharedModule } from 'app/shared/shared.module';
+import { AccueilRightSectionComponent } from './accueil-right-section.component';
+
+@NgModule({
+  imports: [BeOpenMindAppSharedModule],
+  declarations: [AccueilRightSectionComponent],
+  exports: [AccueilRightSectionComponent]
+})
+export class AccueilghtSectionModule {}
